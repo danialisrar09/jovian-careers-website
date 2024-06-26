c5398b7cb922a5fe18188e03b489a71ccb99c05b
CodeByDanial <Danialisrar09@gmail.com>
2024-06-26 21:55:34 +0500

    Added the Flask project

--- app.py
+++ app.py
@@ -2,9 +2,36 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Remote',
+        'Salary': 'Rs. 1,000,000',
+    },
+    {
+        'id': 2,
+        'title': 'Data Scientist',
+        'location': 'Karachi, Pakistan',
+        'Salary': 'Rs. 1,500,000',
+    },
+    {
+        'id': 3,
+        'title': 'Data Engineer',
+        'location': 'Remote',
+        'company': 'Jovian',
+        'Salary': 'Rs. 900,000',
+    },
+    {
+        'id': 4,
+        'title': 'Software Engineer',
+        'location': 'Lahore, Pakistan',
+        'Salary': 'Rs. 800,000',
+    }
+]
 @app.route("/")
-def hello_world():
-    return render_template('home.html')
+def hello_jovian():
+    return render_template('home.html', jobs = JOBS, company = "Jovian")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
